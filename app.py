b00248750c511f950568d8b24129410574647259
Olivianm <144073206+Olivianm@users.noreply.github.com>
2025-05-10 23:54:20 +0000

    model_nutri

--- app.py
+++ app.py
@@ -103,6 +103,4 @@ def predict():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
-    app.run(host="0.0.0.0", port=port, debug=False)
-    
-    
+    app.run(host="0.0.0.0", port=port)
